a913c00257972db2ef57e6e294cb396a361dc86b
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-10-25 18:52:16 +0200

    Доделал ДЗ 10

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './styles.css';
 import './Js-files.js/switcherTheme';
+import menuData from './menu.json';
+import menuMarkup from './data-menu-markup/menu-markup.hbs';
+// console.log(menuMarkup(menuData));  
+const posotionMenuMarkupEl = document.querySelector('ul.js-menu');
+// console.log(posotionMenuMarkupEl);
+createMenuMarkup(menuData);
+
+function createMenuMarkup(menuData) {
+    posotionMenuMarkupEl.insertAdjacentHTML('beforeend', menuMarkup(menuData));
+    // menuMarkup(menuData)
+    
+}
